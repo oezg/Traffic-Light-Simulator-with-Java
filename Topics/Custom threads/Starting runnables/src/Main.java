07d8f5883b1b44b417b5ca50f80bdf9a5c08b5f5
Özgür Yıldırım <oezguer.yildirim@swr.de>
2025-09-15 11:05:47 +0200

    Thread Worker

--- Topics/Custom threads/Starting runnables/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-class Starter {
-
-    public static void startRunnables(Runnable[] runnables) {
-        Arrays.stream(runnables).map(Thread::new).forEach(Thread::start);
-    }
-}
